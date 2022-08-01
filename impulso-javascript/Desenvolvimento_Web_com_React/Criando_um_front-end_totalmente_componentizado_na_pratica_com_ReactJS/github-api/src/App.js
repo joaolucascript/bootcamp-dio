507171c6c228da833522e780242f5dc661d9621f
joaolucascript <zavarash@outlook.com>
2022-08-01 16:27:30 -0300

    criação do ambiente de desenvolvimento

---
+++ impulso-javascript/Desenvolvimento_Web_com_React/Criando_um_front-end_totalmente_componentizado_na_pratica_com_ReactJS/github-api/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Layout from "./components/layout";
+
+function App() {
+  return (
+    <main>
+      <Layout>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/42676546?v=4"
+            alt="User Profile Avatar"
+          />
+          <h1>João Lucas</h1>
+          <h3>Username</h3>
+          <span>joaolucascript</span>
+          <div>
+            <div>
+              <h4>Followers</h4>
+              <span>3</span>
+            </div>
+            <div>
+              <h4>Starreds</h4>
+              <span>7</span>
+            </div>
+            <div>
+              <h4>Followings</h4>
+              <span>10</span>
+            </div>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  );
+}
+
+export default App;
