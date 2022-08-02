b4764b655fe62dc0a02b678b4cf54d2ba8eed944
joaolucascript <zavarash@outlook.com>
2022-08-02 03:02:31 -0300

    apagado linha fullname

--- impulso-javascript/Desenvolvimento_Web_com_React/Criando_um_front-end_totalmente_componentizado_na_pratica_com_ReactJS/github-api/src/components/repository-item/index.js
+++ impulso-javascript/Desenvolvimento_Web_com_React/Criando_um_front-end_totalmente_componentizado_na_pratica_com_ReactJS/github-api/src/components/repository-item/index.js
@@ -5,7 +5,6 @@ const RepositoryItem = ({ name, linkToRepo, fullName }) => {
   return (
     <S.Wrapper>
       <S.WrapperTitle>{name}</S.WrapperTitle>
-      <S.WrapperFullName>full name:</S.WrapperFullName>
       <S.WrapperLink href={linkToRepo} target="_blank" rel="noreferrer">
         {fullName}
       </S.WrapperLink>
